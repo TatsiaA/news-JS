f1d7800cd52a3b48c558250443088e0b528e8ea6
TatsiaA <taniokk@mail.ru>
2021-12-03 13:29:45 +0300

    refactor: add settings for TS and rename files

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'a7af1bca4cc5432986f3bc8001dcc49e', // получите свой ключ https://newsapi.org/
         });
     }
 }
