46e4c34148ff388f0cf829df484f0fb1c939be16
TatsiaA <taniokk@mail.ru>
2021-12-07 02:28:33 +0300

    refactor: change link for API

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'a7af1bca4cc5432986f3bc8001dcc49e', // получите свой ключ https://newsapi.org/
         });
     }
